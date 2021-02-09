c1467e486e547ec92cbb20f301666b19646d5901
A.J. Sethi <u6081208@anu.edu.au>
2021-02-09 14:49:48 +1100

    added desplicing functionality

--- nanoclip/nanoclip.sh
+++ nanoclip/nanoclip.sh
@@ -5,6 +5,7 @@ export wtBam="/g/data/lf10/as7425/2020-11-05_feloniusGru/analysis/2021-02-02_nik
 export modBam="/g/data/lf10/as7425/2020-11-05_feloniusGru/analysis/2021-02-02_nikolay-HL-1-crosslink/Nick_LKL-SQK-RNA002-XL/Nick_LKL-SQK-RNA002-XL.primary.sorted.bam"
 export window="5:134900000-149900000"
 export outDir="/g/data/lf10/as7425/2020-11-05_feloniusGru/analysis/2021-02-08_build-nanoclip"
+export myGenome="/g/data/lf10/as7425/genomes/mouse_genome/Mus_musculus.GRCm38.dna.primary_assembly..genome"
 
 mkdir -p ${output}
 
@@ -21,9 +22,31 @@ echo "done for all"
 wtFilt="/g/data/lf10/as7425/2020-11-05_feloniusGru/analysis/2021-02-08_build-nanoclip/window_Nick_LK1-SQK-RNA002-NXL.primary.sorted.bam"
 modFilt="/g/data/lf10/as7425/2020-11-05_feloniusGru/analysis/2021-02-08_build-nanoclip/window_Nick_LKL-SQK-RNA002-XL.primary.sorted.bam"
 
+# desplit the data
+for i in $wtBam $modBam
+  do bedtools bamtobed -split -i ${outDir}/window_${i##*/} | bedtools bedtobam -ubam -g ${myGenome} -i - | samtools sort > ${outDir}/desplice_${i##*/}
+  samtools index ${outDir}/desplice_${i##*/}
+done
+
+# export desplice names for pileup
+wtDesplice="/g/data/lf10/as7425/2020-11-05_feloniusGru/analysis/2021-02-08_build-nanoclip/desplice_Nick_LK1-SQK-RNA002-NXL.primary.sorted.bam"
+modDesplice="/g/data/lf10/as7425/2020-11-05_feloniusGru/analysis/2021-02-08_build-nanoclip/desplice_Nick_LKL-SQK-RNA002-XL.primary.sorted.bam"
+
 # do mpileup
-time samtools mpileup -d 0 -f ${myFasta} -B ${wtFilt} ${modFilt} -C 20 -Q 7 | awk '$7>0{print}'> ${outDir}/2021-02-08_filtLib_reducedPileup.txt
-gzip ${outDir}/2021-02-08_filtLib_reducedPileup.txt
+time samtools mpileup -d 0 -f ${myFasta} -B ${wtDesplice} ${modDesplice} -C 0 -Q 0 -q 0 | awk '$7>0{print}'> ${outDir}/2021-02-09_desplice_filtLib_reducedPileup.txt
+gzip ${outDir}/2021-02-09_desplice_filtLib_reducedPileup.txt
+
+
+##################################################
+##################################################
+##################################################
+################################################## testing region
+################################################## testing region
+################################################## testing region
+
+bedtools bamtobed -split -i /g/data/lf10/as7425/2020-11-05_feloniusGru/analysis/2021-02-08_build-nanoclip/window_Nick_LKL-SQK-RNA002-XL.primary.sorted.bam | bedtools bedtobam -ubam -g ${myGenome} -i - | samtools sort -l 0 > ${outDir}/desplice_${i##*/}
+
+
 
 
 
