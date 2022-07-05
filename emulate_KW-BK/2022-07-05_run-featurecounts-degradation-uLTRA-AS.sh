d707305f4f632171ed7c9d5c63da8c519552f394
AJ local <AJlocal@JCSMR-041471MD.local>
2022-07-05 15:14:12 +1000

    updated script to quantify gene expression counts for Agin's degraded data (aligned to genome using uLTRA

--- emulate_KW-BK/2022-07-05_run-featurecounts-degradation-uLTRA-AS.sh
+++ emulate_KW-BK/2022-07-05_run-featurecounts-degradation-uLTRA-AS.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
+# written by AJ Sethi on 2022-05-11
+# aim: Quantify gene expression in uLTRA genomic alignments of Agin's degradation data using subread featurecounts
+# rus on AJ's iMac
+
+############################################################
+############################################################
+
 export annotation="/Users/AJlocal/localGadiData/0_a_tmp/Homo_sapiens.GRCh38.104.chr.gtf"
 # brew install brewsci/bio/subread
 cd /Users/AJlocal/localGadiData/2022-06-22_HEK293-degradation-first4-AR_liqa-genome-alignments_BK
-featureCounts --primary -L -s 1 -a ${annotation} `ls *bam` -o fc.txt
+
+featureCounts --primary -L -s 1 -a ${annotation} --extraAttributes "gene_name" `ls *bam` -o 2022-07-05_ARdegradation-genomic-featurecounts-AS.txt
