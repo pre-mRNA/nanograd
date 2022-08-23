c913ea61edefbd23c3ed716c4d0da9fdf2d2f073
AJ local <AJlocal@JCSMR-041471MD.local>
2022-08-23 15:16:00 +1000

    update nanograd5 plots and make heatmap for transcript integrity scores

--- rna-degradation-rates/2022-08-23_merge-nanograd5-first6.R
+++ rna-degradation-rates/2022-08-23_merge-nanograd5-first6.R
@@ -23,7 +23,7 @@ import_tin <- function(dataPath, mySample, myCondition){
   # read the tsv 
   read_tsv(dataPath, col_names = T, col_types = "fddfffddddd") %>% 
     dplyr::rename(tin = df_ratio) %>% 
-    dplyr::filter(cov > 20) %>% 
+    dplyr::filter(cov > 5) %>% 
     dplyr::select(transcript_id, tin) %>% 
     mutate(condition = myCondition, sample = mySample)
 }
@@ -58,12 +58,64 @@ nrow(tin_matrix_complete)
 # transcript level correlation plot 
 library(corrplot)
 
+# make correlation plots for common transcripts 
 dev.new(width = 9, heigth = 9, noRStudioGD = T, unit = "cm")
-
 tin_matrix_complete %>% 
   select(-transcript_id) %>% 
   cor(., method = "spearman") %>% 
   corrplot(., method = "pie", type = "full", order = "original", tl.col = "black", cl.lim=c(0,1), diag = F, addrect = T)
+
+# check the correlation between libraries 
+cor_data <- tin_matrix_complete %>% 
+  select(-transcript_id) %>% 
+  cor(., method = "spearman") 
+
+###########################
+
+# pool the replicates 
+
+# write a function to import data 
+import_tin <- function(dataPath, mySample, myCondition){
+  
+  # read the tsv 
+  read_tsv(dataPath, col_names = T, col_types = "fddfffddddd") %>% 
+    dplyr::rename(tin = df_ratio) %>% 
+    dplyr::select(transcript_id, tin, cov) %>% 
+    mutate(condition = myCondition, sample = mySample)
+}
+
+# read in the data 
+merged_tin <- bind_rows(import_tin("~/localGadiData/2022-08-23_nanograd5-degradation-first6/all.undegraded_hek293_pass1.fastq.gz.sorted_nanograd5_out.txt.tmp", "control_rep1", "undegraded"),
+                        import_tin("~/localGadiData/2022-08-23_nanograd5-degradation-first6/all.undegraded_hek293_pass2.fastq.gz.sorted_nanograd5_out.txt.tmp", "control_rep2", "undegraded"),
+                        import_tin("~/localGadiData/2022-08-23_nanograd5-degradation-first6/mild_degradataion_rep1.fastq_nanograd5_out.txt.tmp", "mild_rep1", "mildly_degraded"),
+                        import_tin("~/localGadiData/2022-08-23_nanograd5-degradation-first6/mild_degradataion_rep2.fastq_nanograd5_out.txt.tmp", "mild_rep2", "mildly_degraded"),
+                        import_tin("~/localGadiData/2022-08-23_nanograd5-degradation-first6/all.5mM_MgCl_degrdation_pass1.fastq.gz.sorted_nanograd5_out.txt.tmp", "heavy_rep1", "heavy_degradation"),
+                        import_tin("~/localGadiData/2022-08-23_nanograd5-degradation-first6/all.5mM_MgCl_degrdation_pass2.fastq.gz.sorted_nanograd5_out.txt.tmp", "heavy_rep2", "heavy_degradation"))
+
+# take the average per condition 
+merged_per_condition_tin <- merged_tin %>% 
+  group_by(transcript_id, condition) %>% 
+  summarise(tin = weighted.mean(tin, cov), sum_cov = sum(cov)) %>%
+  filter(sum_cov > 10) %>% 
+  select(-sum_cov) %>% 
+  pivot_wider(names_from = condition, values_from = tin) %>% x
+  na.omit() %>% 
+  dplyr::select(transcript_id, undegraded, mildly_degraded, heavy_degradation)
+
+merged_per_condition_tin # 3506 transcripts 
+
+###########################
+
+# convert to matrix and make a heatmap 
+merged_matrix <- merged_per_condition_tin %>% ungroup() %>% select(-transcript_id) %>% as.matrix()
+rownames(merged_matrix) <- merged_per_condition_tin$transcript_id
+
+
+# perform clustering and make a heatmap 
+dev.new(width = 12, height = 12, noRStudioGD = T, unit = "cm")
+Heatmap(merged_matrix)
+
+
 
 
 
