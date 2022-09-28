28f3e7c63ea511c942809e252bc6e12493d4dfad
Bhavika Kumar <bk9031@gadi-login-05.gadi.nci.org.au>
2022-09-28 15:01:54 +1000

    added updated nanopolish script

--- analysis_BK/2022-09-23_nanopolish.sh
+++ analysis_BK/2022-09-23_nanopolish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # written by BK on 2022-09-23
-# aim: script for nanopolish 
+# aim: script for nanopolish
 
 #############################################################
 #############################################################
@@ -9,14 +9,8 @@
 # getting data
 export data="/g/data/xc17/degradation_project/Mg_degraded/data"
 
-# installing nanopolish
-export install_nanopolish="/g/data/xc17/bk9031/apps"
-
-cd ${install_nanopolish}
-
-git clone --recursive https://github.com/jts/nanopolish.git
-cd nanopolish
-make
+# nanopolish path
+export PATH="${PATH}:/g/data/xc17/as7425/sharing/apps/nanopolish"
 
 #  data preprocessing
 runNano() {
@@ -25,27 +19,27 @@ runNano() {
   local fastq=$3
   local alignment=$4
   local genome=$5
+  local outputDir=$6
   echo "$(data)....starting indexing for nanopolish"
   nanopolish index -s ${seq_sum} -d ${fast5} ${fastq} || echo "$(date)....nanopolish indexing failed"
   echo "$(date)....done indexing for nanopolish"
 
   echo "$(data)....starting alignment for nanopolish"
-  nanopolish eventalign --reads ${fastq} --bam ${alignment} --genome ${genome} || echo "$(date)....nanopolish alignment failed"
+  nanopolish eventalign --reads ${fastq} --bam ${alignment} --genome ${genome} -v -t 48 --scale-events --print-read-names --samples > ${outputDir} || echo "$(date)....nanopolish alignment failed"
   echo "$(date)....done alignment for nanopolish"
 }; export -f runNano
 
 # defining path of genome
 export genome="/g/data/xc17/bk9031/genomes/human_genome/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa"
 
-# example command
-runNano ${sum_wt_1} ${fast5_wt_1} ${fastq_wtt_1} ${bam_wt_1} ${genome}
+# defining output path
+export outputDir="/g/data/xc17/bk9031/2022_nanograd_bk/analysis/2021_HEK293-degradation-first4-AR/nanopoilish"
 
 # for sample wt_1
 sum_wt_1= "/g/data/xc17/degradation_project/Mg_degraded/data/undegraded_hek293_pass1/sequencing_summary_FAQ86281_15c37cc7.txt"
 fast5_wt_1="/g/data/xc17/degradation_project/Mg_degraded/data/undegraded_hek293_pass1"
 fastq_wt_1="/g/data/xc17/degradation_project/Mg_degraded/basecalled/undegraded_hek293_pass1/all.undegraded_hek293_pass1.fastq.gz"
 bam_wt_1="/g/data/xc17/bk9031/2022_nanograd_bk/analysis/2021_HEK293-degradation-first4-AR/primaryAlignments/undegraded_hek293_pass1_primary.bam"
-genome="/g/data/xc17/bk9031/genomes/human_genome/Homo_sapiens.GRCh38.dna_sm.primary_assembly.fa"
 
 # for sample wt_2
 sum_wt_2="/g/data/xc17/degradation_project/Mg_degraded/data/undegraded_hek293_pass2/sequencing_summary_FAP73818_93b719e9.txt"
@@ -77,4 +71,8 @@ fast5_deg2="/g/data/xc17/degradation_project/Mg_degraded/data/5mM_MgCl_degrdatio
 fastq_deg2="/g/data/xc17/bk9031/2022_nanograd_bk/data/2021_HEK293-degradation-first4-AR/5mM_MgCl_degrdation_pass2/all.5mM_MgCl_degrdation_pass2.fastq.gz"
 bam_deg2="/g/data/xc17/bk9031/2022_nanograd_bk/data/2021_HEK293-degradation-first4-AR/5mM_MgCl_degrdation_pass2/all.5mM_MgCl_degrdation_pass2.fastq.gz.sorted.bam"
 
+# running nanopolish for one sample
+runNano ${sum_wt_1} ${fast5_wt_1} ${fastq_wtt_1} ${bam_wt_1} ${genome}
 
+for i in ${data}; do runNano ${i} & done
+wait && echo "done for all"
